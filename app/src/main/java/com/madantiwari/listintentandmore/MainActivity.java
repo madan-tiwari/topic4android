055db81aa575cdd32f60b09c92489afa8902c6a3
madan-tiwari <rebelmadan13@gmail.com>
2019-10-25 11:12:32 +0545

    hashmap

--- app/src/main/java/com/madantiwari/listintentandmore/MainActivity.java
+++ app/src/main/java/com/madantiwari/listintentandmore/MainActivity.java
@@ -3,16 +3,26 @@ package com.madantiwari.listintentandmore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
      public static final String countries[]={
-             "Nepal",
-             "India",
-             "China"
+             "Nepal", "Kathmandu",
+             "India", "New Delhi",
+             "China", "Beijing",
+             "UK", "London",
+             "Canada","Ottawa"
      };
     ListView lvcountries;
+    private Map<String, String> countriesMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +30,29 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvcountries = findViewById(R.id.lvCountries);
 
-        ArrayAdapter<String> countryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1
-        , countries);
+        countriesMap = new HashMap<>();
+        for(int i=0;i<countries.length; i+=2){
+            countriesMap.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(countriesMap.keySet())
+        );
+
         lvcountries.setAdapter(countryAdapter);
+
+        lvcountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                String capital = countriesMap.get(country);
+
+                Toast.makeText(MainActivity.this, "Capital of "+country+" is: "+capital,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
 }
